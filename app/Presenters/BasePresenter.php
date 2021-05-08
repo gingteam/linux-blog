4c0a7cd60216d17e091dd0db2202dbfaf3b7af0e
ging-dev <gingdz11012001@gmail.com>
2021-05-08 17:15:30 +0700

    Fixes

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -12,17 +12,24 @@ class BasePresenter extends Nette\Application\UI\Presenter
 {
     protected $title = 'Linux Blog';
 
+    /** @var Explorer */
     protected $database;
 
-    public function __construct(Explorer $database)
+    public function __construct()
     {
+        parent::__construct();
         $this->absoluteUrls = true;
+    }
+
+    public function injectDatabase(Explorer $database)
+    {
         $this->database = $database;
     }
 
     protected function afterRender()
     {
         $this->template->title = $this->title;
+        parent::afterRender();
     }
 
     protected function createComponentBreadCrumb()
--- app/Presenters/Error4xxPresenter.php
+++ app/Presenters/Error4xxPresenter.php
@@ -6,7 +6,7 @@ namespace App\Presenters;
 
 use Nette;
 
-final class Error4xxPresenter extends Nette\Application\UI\Presenter
+final class Error4xxPresenter extends BasePresenter
 {
     public function startup(): void
     {
@@ -18,6 +18,7 @@ final class Error4xxPresenter extends Nette\Application\UI\Presenter
 
     public function renderDefault(Nette\Application\BadRequestException $exception): void
     {
+        $this->title = $exception->getMessage();
         // load template 403.latte or 404.latte or ... 4xx.latte
         $file = __DIR__."/templates/Error/{$exception->getCode()}.latte";
         $this->template->setFile(is_file($file) ? $file : __DIR__.'/templates/Error/4xx.latte');
